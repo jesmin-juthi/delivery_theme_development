151942a770231544a9add95640200145870f3a68
jesmin-juthi <thesathil67@gmail.com>
2022-10-25 01:27:08 +0600

    Files and some functions added

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+    function delivery_theme_setup()
+    {
+        load_theme_textdomain("delivery");
+        add_theme_support( "title-tag" );
+        add_theme_support( "menus" );
+    }
+add_action("after_setup_theme", "delivery_theme_setup");
+
+    function delivery_assets_files()
+    {
+        // CSS Files
+        wp_enqueue_style("delivery-theme-icons", "//cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css");
+        wp_enqueue_style("delivery-theme-stylesheet", get_template_directory_uri() . "/assets/css/styles.css");
+
+        // JS Files
+        wp_enqueue_script("delivery-theme-main-js", get_template_directory_uri() . "/assets/js/main.js", null, "1.0.0", true);
+    }
+
+add_action("wp_enqueue_scripts","delivery_assets_files");

1b7d9a3a92a5fa23804fe2b21cffd8e55ee90230
jesmin-juthi <thesathil67@gmail.com>
2022-10-26 02:30:38 +0600

    colors and hero image added

--- inc/option-panel/customizer.php
+++ inc/option-panel/customizer.php
@@ -16,6 +16,16 @@
 			"section"	=> "delivery_theme_option",
 			"settings"	=> "header_title_settings"
 		]);
+		// header title color 
+		$wp_customize->add_setting("header_title_color", [
+			"transport"	=> "refresh",
+			"type"		=> "theme_mod"
+		]);
+		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, "header_title_color_c",[
+			"label"		=> "Header Title Color",
+			"section"	=> "delivery_theme_option",
+			"settings"	=> "header_title_color"
+		]));
 		// header description
 		$wp_customize->add_setting("header_desc_s", [
 			"default"	=> "Order your favorite foods at any time and we will deliver them right to where you are.",
@@ -28,6 +38,16 @@
 			"section"	=> "delivery_theme_option",
 			"settings"	=> "header_desc_s"
 		]);
+		// header description color 
+		$wp_customize->add_setting("header_desc_color", [
+			"transport"	=> "refresh",
+			"type"		=> "theme_mod"
+		]);
+		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, "header_desc_color_c",[
+			"label"		=> "Header Description Color",
+			"section"	=> "delivery_theme_option",
+			"settings"	=> "header_desc_color"
+		]));
 		// header button 
 		$wp_customize->add_setting("header_btn_s", [
 			"default"	=> "Get Started",
@@ -40,6 +60,36 @@
 			"section"	=> "delivery_theme_option",
 			"settings"	=> "header_btn_s"
 		]);
+		// header button color 
+		$wp_customize->add_setting("header_btn_color", [
+			"transport"	=> "refresh",
+			"type"		=> "theme_mod"
+		]);
+		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, "header_btn_color_c",[
+			"label"		=> "Header Button Color",
+			"section"	=> "delivery_theme_option",
+			"settings"	=> "header_btn_color"
+		]));
+		// header button bg color 
+		$wp_customize->add_setting("header_btn_bg", [
+			"transport"	=> "refresh",
+			"type"		=> "theme_mod"
+		]);
+		$wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, "header_btn_bg_c",[
+			"label"		=> "Header Button Background Color",
+			"section"	=> "delivery_theme_option",
+			"settings"	=> "header_btn_bg"
+		]));
+		// hero img
+		$wp_customize->add_setting("hero_img", [
+			"transport"	=> "refresh",
+			"type"		=> "theme_mod"
+		]);
+		$wp_customize->add_control(new WP_Customize_Media_Control($wp_customize, "hero_img_c",[
+			"label"		=> "Hero Image",
+			"section"	=> "delivery_theme_option",
+			"settings"	=> "hero_img"
+		]));
 		// about header
 		$wp_customize->add_setting("about_header_s", [
 			"default"	=> "Find Out A Little More About Us",
